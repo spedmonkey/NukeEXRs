6802a7f0abd286d922b749bee46425673470b76b
spedmonkey <spedcr@gmail.com>
2015-06-23 20:41:14 +1200

    myFirst commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='NukeEXRs',
+    version='001',
+    packages=[''],
+    url='',
+    license='',
+    author='spedmonkey',
+    author_email='spedcr@gmail.com',
+    description='just testing shit'
+)
